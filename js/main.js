2cc925bd52881d5513f82a0fc4a03e9af7bad8cd
blastz <touchzh1996@gmail.com>
2017-06-24 22:44:31 +0800

    Git init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('#load').click(function() {
+        $(document).load("novels/chapter_1.txt", function(responseTxt) {
+            var lines = responseTxt.split('\r');
+            for(var i=0; i<lines.length; i++) {
+                if(i === 0) {
+                    $('#title').text(lines[i]);
+                } else {
+                    $('#content').append('<p>' + lines[i] + '</p>');
+                }
+            }
+        });
+    })
+})
